c335d161a4e498e9edf0269a96059b785561e672
mcglade8 <78363682+mcglade8@users.noreply.github.com>
2024-05-03 15:18:42 -0400

    Updated formatting

--- virtual_resume.js
+++ virtual_resume.js
@@ -22,15 +22,15 @@ function hideExperience(e){
 
 function toggleThis(e){
     var children = e.children;
-    var icon = children[0].getElementsByTagName("i")[0];
+    var icon = e.getElementsByTagName("i")[0];
     var accomplishments = children[children.length-1];
     if(accomplishments.style.display === "none"){
         accomplishments.style.display = "block";
-        icon.classList.remove("fa-angle-down");
-        icon.classList.add("fa-angle-up");
+        icon.classList.remove("fa-angle-right");
+        icon.classList.add("fa-angle-down");
     }else{
         accomplishments.style.display = "none";
-        icon.classList.remove("fa-angle-up");
-        icon.classList.add("fa-angle-down");
+        icon.classList.remove("fa-angle-down");
+        icon.classList.add("fa-angle-right");
     }
 }
